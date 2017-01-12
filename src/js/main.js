f63f7c9d926ddbb21938ca1ab13e2e0184a6fdff
Yuri Ramos <yuri.pramos@gmail.com>
2017-01-12 16:54:57 -0200

    switching filtered methods

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import "../css/main.css"
+import React from "react"
+import ReactDOM from "react-dom"
+import TodoStore from "./TodoStore"
+import TodoList from "./TodoList"
+
+const app = document.getElementById("app")
+
+ReactDOM.render(<TodoList store={TodoStore} />, app)
